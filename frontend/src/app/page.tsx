0eb831f5c68eb06f3fc784259e9f33091e3d56e5
ar-ease <arghya0017@gmail.com>
2025-07-15 01:25:26 +0530

    frontend init

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <Button>hello</Button>
+    </div>
+  );
+}
